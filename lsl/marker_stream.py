c250dabe12b0a9f36b9f94436cc19def1fffb408
SethHu20 <sethhu20@gmail.com>
2022-08-31 15:13:09 +1000

    Added marker marker_stream

--- lsl/marker_stream.py
+++ lsl/marker_stream.py
@@ -11,40 +11,47 @@ info = StreamInfo(
     'Markers',
     1,
     IRREGULAR_RATE,
-    'string',
+    'int32',
     'eegmarker1'
 )
 
+index = 1000
+
 outlet = StreamOutlet(info)
 
-print("Marker Streaming ready")
+print("Marker Streaming ready...")
 
-def push(marker_name: str):
-    outlet.push_sample([marker_name])
-
-
-def main():
-    # first create a new stream info (here we set the name to MyMarkerStream,
-    # the content-type to Markers, 1 channel, irregular sampling rate,
-    # and string-valued data) The last value would be the locally unique
-    # identifier for the stream as far as available, e.g.
-    # program-scriptname-subjectnumber (you could also omit it but interrupted
-    # connections wouldn't auto-recover). The important part is that the
-    # content-type is set to 'Markers', because then other programs will know how
-    #  to interpret the content
-    info = StreamInfo('MyMarkerStream', 'Markers', 1, 0, 'string', 'myuidw43536')
-
-    # next make an outlet
-    outlet = StreamOutlet(info)
-
-    print("now sending markers...")
-    markernames = ['Test', 'Blah', 'Marker', 'XXX', 'Testtest', 'Test-1-2-3']
-    while True:
-        # pick a sample to send an wait for a bit
-        outlet.push_sample([random.choice(markernames)])
-        time.sleep(random.random() * 3)
+# while True:
+#     message = [index]
+#     outlet.push_sample(message)
+#     print(message)
+#     index += 1
+#     time.sleep(1)
 
 
-if __name__ == '__main__':
-    # main()
-    pass
+
+# def main():
+#     # first create a new stream info (here we set the name to MyMarkerStream,
+#     # the content-type to Markers, 1 channel, irregular sampling rate,
+#     # and string-valued data) The last value would be the locally unique
+#     # identifier for the stream as far as available, e.g.
+#     # program-scriptname-subjectnumber (you could also omit it but interrupted
+#     # connections wouldn't auto-recover). The important part is that the
+#     # content-type is set to 'Markers', because then other programs will know how
+#     #  to interpret the content
+#     info = StreamInfo('MyMarkerStream', 'Markers', 1, 0, 'string', 'myuidw43536')
+
+#     # next make an outlet
+#     outlet = StreamOutlet(info)
+
+#     print("now sending markers...")
+#     markernames = ['Test', 'Blah', 'Marker', 'XXX', 'Testtest', 'Test-1-2-3']
+#     while True:
+#         # pick a sample to send an wait for a bit
+#         outlet.push_sample([random.choice(markernames)])
+#         time.sleep(random.random() * 3)
+
+
+# if __name__ == '__main__':
+#     # main()
+#     pass
